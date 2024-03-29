79dd197d95d6f0edc620bee9091e2f3c3844cc15
Ancita Andrade <ancitaandrade@gmail.com>
2024-03-29 13:56:01 -0400

    Text Summarization

---
+++ Recapify_API/Utility/FileHelper.py
@@ -0,0 +1,23 @@
+import PyPDF2
+import docx
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'txt', 'pdf', 'docx'}
+
+def extract_text_from_txt(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+def extract_text_from_pdf(file_path):
+    pdf_reader = PyPDF2.PdfReader(file_path)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+def extract_text_from_docx(file_path):
+    doc = docx.Document(file_path)
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text
+    return text
