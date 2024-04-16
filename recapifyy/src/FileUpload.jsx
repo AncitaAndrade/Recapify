381953ae17df559615160034b139cc0776a6a5f8
Pavangogineni18 <79048420+Pavangogineni18@users.noreply.github.com>
2024-04-16 09:54:05 -0400

    ui fixes1

--- recapifyy/src/FileUpload.jsx
+++ recapifyy/src/FileUpload.jsx
@@ -1,10 +1,15 @@
 // FileUpload.jsx
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import LinearProgress from '@mui/material/LinearProgress';
 
 function FileUpload({ onSummaryGenerated }) {
   const [file, setFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(''); 
+  const [progress, setProgress] = useState(0);
+  
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -17,10 +22,16 @@ function FileUpload({ onSummaryGenerated }) {
       if (isValidFileType) {
         const formData = new FormData();
         formData.append('file', file);
+        setUploading(true); 
+
         try {
           const response = await fetch('http://recapify.us-east-2.elasticbeanstalk.com/summarize', {
             method: 'POST',
             body: formData,
+            onUploadProgress: (progressEvent) => {
+              const progressPercent = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+              setProgress(progressPercent); 
+            },
           });
           if (!response.ok) {
             throw new Error('Failed to upload file');
@@ -30,12 +41,21 @@ function FileUpload({ onSummaryGenerated }) {
         } catch (error) {
           console.error('Error uploading file:', error);
         }
+        finally {
+          setUploading(false); 
+          setProgress(0);
+        }
+
       } else {
         displayErrorMessage(`Error: File '${file.name}' is not a supported file type. Allowed file types are: wav, mp3, aac, ogg, mpeg, amr, m4a, mp4, flac, pdf, docx, txt`); // Added
-      }
-    } else {
+      } 
+      
+    }
+    
+    else {
       displayErrorMessage('Error: No file selected'); // Added
     }
+    
   };
 
   const checkFileType = (fileName) => {
@@ -49,20 +69,13 @@ function FileUpload({ onSummaryGenerated }) {
   };
 
   return (
-    <div style={{  width: '600px',
-    padding: '20px',
-    backgroundColor: '#f5f5f5',
-    border: '2px dashed #333',
-    borderRadius: '8px',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    fontFamily: 'Arial, sans-serif',
-    fontSize: '16px',
-    color: '#333',
-    textAlign: 'center'}}>
+    <div className="file-upload-container">
       <input type="file" onChange={handleFileChange} />
       <Button variant="contained" color="primary" onClick={handleUpload}>
         Upload
       </Button>
+      {uploading && <CircularProgress />}
+      <LinearProgress variant="determinate" value={progress} />
       {errorMessage && <p>{errorMessage}</p>}
     </div>
   );
